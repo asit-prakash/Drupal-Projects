8dd188de3b7c721f02f2e144e80d64ca8c5a48f1
asit-prakash <asit.prakash@innoraft.com>
2020-04-01 18:21:14 +0530

    austrailian government files

---
+++ aus-gov/web/themes/custom/ausgov_barrio/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ausgov_barrio = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
